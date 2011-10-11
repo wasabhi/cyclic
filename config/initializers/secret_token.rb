d78e331ea2fbc530573df207032483393293fca4
wasabhi <abhinav.keswani@trineo.co.nz>
2011-10-11 21:57:28 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclic::Application.config.secret_token = '4923ceb78d901ebd89e6cddad6ddbbea5b8bc91d6ead8850e4d041d902a62642c97bc6fa60ff88a931d32a5c655382896c1258af41bfcdd582a2ba31661cf796'
