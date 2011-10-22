422db4f7ee3d8e52142516a6914c3cf604cc2143
wasabhi <abhinav.keswani@trineo.co.nz>
2011-10-22 23:48:42 +1300

    User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new
+user.email = 'user@example.com'
+user.password = user.password_confirmation = 'password'
+user.save
+user.confirm!
