26ccca2277c58eb23a5aafc1af474e488cdf9833
wasabhi <abhinav.keswani@trineo.co.nz>
2011-11-09 22:33:22 +1300

    Favourite rides must have a name

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,11 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :distance, :date
+  validate :favourite_rides_must_have_a_name
+
+  def favourite_rides_must_have_a_name
+    if favourite && name.blank?
+      errors.add(:name, "A favourite ride must have a name")
+    end
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,10 @@ describe Ride do
   it { should belong_to :user }
   it { should validate_presence_of :date }
   it { should validate_presence_of :distance }
+
+  specify "a favourite ride should always have a name" do
+    subject.favourite = true
+    subject.name = nil
+    subject.should_not be_valid
+  end
 end
